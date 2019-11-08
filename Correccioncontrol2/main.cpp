e7e3fc2eeeb5a52a66b37a5b3ba14096e3a4c170
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-11-07 21:42:10 -0500

    Update main.cpp

--- Correccioncontrol2/main.cpp
+++ Correccioncontrol2/main.cpp
@@ -7,50 +7,35 @@ using namespace std;
 
 int main()
 {
-    Estudiante e1("sebastian","paz","14523");
-    Estudiante e2("diego","cardenas","36178");
+    Estudiante e2("sebastian","paz","14523"),e3("diego","cardenas","36178");
+    Estudiante e1[2]={Estudiante(e2), Estudiante(e3)};
 
-    e1.Obtener_nombre();
-    e1.Obtener_apellido();
-    e1.Obtener_codigo();
-
-    e2.Obtener_nombre();
-    e2.Obtener_apellido();
-    e2.Obtener_codigo();
+    cout<<e2.Obtener_nombre()<<" "<<e2.Obtener_apellido()<<" "<<e2.Obtener_codigo()<<endl;
+    cout<<e3.Obtener_nombre()<<" "<<e3.Obtener_apellido()<<" "<<e3.Obtener_codigo()<<endl<<endl;
 
 
-    Curso c1(28,"Lucas","22154");
-    c1.Obtener_nombre();
-    c1.Obtener_codigo();
-    c1.Obtener_cantidadAlumnos();
-    c1.Incrementa_Nestudiantes();
+
+    Curso c1(100,"Lucas","22154",e1);
+    cout<<"nombre: "<<c1.Ob_nombre()<<endl;
+    cout<<"codigo: "<<c1.Ob_codigo()<<endl;
+    cout<<"cantidad de alumnos: "<<c1.Ob_cantidadAlumnos()<<endl;
+    cout<<"numero de estudiantes: "<<c1.In_Nestudiantes()<<endl<<endl;
+ //   cout<<c1.Ob_direccion();
 
     Curso c2=c1;
-    c2.Obtener_nombre();
-    c2.Obtener_codigo();
-    c2.Obtener_cantidadAlumnos();
-    c2.Incrementa_Nestudiantes();
+    cout<<"nombre: "<<c2.Ob_nombre()<<endl;
+    cout<<"codigo: "<<c2.Ob_codigo()<<endl;
+    cout<<"cantidad alumnos: "<<c2.Ob_cantidadAlumnos()<<endl;
+    cout<<"numero de estudiantes: "<<c2.In_Nestudiantes()<<endl<<endl;
+
+    Estudiante *ptr=e1;
+    cout<<"nombre: "<<ptr->Obtener_nombre()<<endl;
+    cout<<"apellido: "<<ptr->Obtener_apellido()<<endl;
+    cout<<"codigo: "<<ptr->Obtener_codigo()<<endl<<endl;
+    ptr++;
+    cout<<"nombre: "<<ptr->Obtener_nombre()<<endl;
+    cout<<"apellido: "<<ptr->Obtener_apellido()<<endl;
+    cout<<"codigo: "<<ptr->Obtener_codigo()<<endl<<endl;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
